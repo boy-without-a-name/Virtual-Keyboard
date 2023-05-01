1e978ec49ad7618e2d8277ee109f2edb4a022b26
boy without a name <murenkov95@gmail.com>
2023-05-01 22:56:30 +0300

    refactor: fix text

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ description.innerHTML='Клавиатура создана в операцион
 wrapperDiv.append(description)
 
 const lang =createNode('p','language');
-lang.innerHTML='Для переключения языка комбинация: левыe ctrl + alt'
+lang.innerHTML='Для переключения языка комбинация: ctrl + alt'
 wrapperDiv.append(lang)
 
 const DigitRowKey =['Backquote','Digit1','Digit2','Digit3','Digit4','Digit5','Digit6','Digit7','Digit8','Digit9','Digit0','Minus','Equal','Backspace'];
